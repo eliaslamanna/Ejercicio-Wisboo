587d215e536a5e2442a9bdc0bb7d3a97e9595dc3
Elias Lamanna <eliaslamanna@gmail.com>
2021-03-18 04:01:43 -0300

    cambios visuales

--- app/buscarImagenes/buscarImagenesCtrl.js
+++ app/buscarImagenes/buscarImagenesCtrl.js
@@ -1,8 +1,6 @@
 var buscarImagenes = angular.module('buscarImagenesApp',[]);
 
 buscarImagenes.controller('buscarImagenesCtrl', ['$scope','$http','$rootScope', function($scope,$http,$rootScope) {
-    $scope.inputBusqueda;
-    $scope.listaImagenes;
     
     $scope.fetch = function(){
       $http.get('https://api.unsplash.com/search/photos?query=' + $scope.inputBusqueda + '&client_id=bJhE54az5ubJOXvHNtLFBZoUfpZ6VLQgyJ-sh7xp0WQ')
@@ -13,6 +11,7 @@ buscarImagenes.controller('buscarImagenesCtrl', ['$scope','$http','$rootScope', 
 
     $scope.guardarImagen = function(urlImagen) {
       $rootScope.imagenesGuardadas.push(urlImagen);
+      alert("Se guardo la imagen");
     }
 
     
