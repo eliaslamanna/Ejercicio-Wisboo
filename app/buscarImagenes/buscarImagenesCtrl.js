cdbab2f824a4a445f995deb3b2eac7c03f6c50d7
Elias Lamanna <eliaslamanna@gmail.com>
2021-03-18 21:38:25 -0300

    fix alert guardar y nuevo boton mostrar mas sin estilos

--- app/buscarImagenes/buscarImagenesCtrl.js
+++ app/buscarImagenes/buscarImagenesCtrl.js
@@ -2,17 +2,23 @@ var buscarImagenes = angular.module('buscarImagenesApp',[]);
 
 buscarImagenes.controller('buscarImagenesCtrl', ['$scope','$http','$rootScope', function($scope,$http,$rootScope) {
     
+    $scope.limite = 0;
+
+    $scope.agrandar = function() {
+      $scope.limite += 3;
+    }
+
     $scope.fetch = function(){
       $http.get('https://api.unsplash.com/search/photos?query=' + $scope.inputBusqueda + '&client_id=bJhE54az5ubJOXvHNtLFBZoUfpZ6VLQgyJ-sh7xp0WQ')
       .then(function(response){
         $scope.listaImagenes = response.data.results;
+        $scope.limite += 3;
       });
     }
 
     $scope.guardarImagen = function(imagen) {
       if ($rootScope.imagenesGuardadas.indexOf(imagen.urls.thumb) == -1){
         $rootScope.imagenesGuardadas.push(imagen.urls.thumb);
-        alert("Se guardo la imagen");
         imagen.liked_by_user = true;
       }
       else {
